b9f4df3d45166c6880bf01820422f459a5110841
feruricar <feruricar>
2022-07-28 16:19:33 -0300

    Editar Remover e Filtro

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -1,9 +1,15 @@
 
+from datetime import date
 from django import forms
 from django.forms import fields, models
 from .models import Pessoa
 
 class PessoaForm(forms.ModelForm):
+    data_nacimento = forms.DateField(
+        widget=forms.TextInput(
+            attrs={"type":"date"}
+        )
+    )
     class Meta:
         model = Pessoa
         fields = ['nome_completo', 'data_nacimento','ativa']
